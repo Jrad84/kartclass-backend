fef4f51715e9cc6d6db3ca856211c82223fa0363
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-03-01 11:10:43 +1100

    Update models.py

--- kc/core/models.py
+++ kc/core/models.py
@@ -77,7 +77,7 @@ class Category(models.Model):
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
-    longdescription = models.TextField(max_length=5000, null=True)    
+    longdescription = models.TextField(max_length=9000, null=True)    
     description = models.CharField(max_length=150, null=True)
     category = models.ManyToManyField(Category, related_name='category')
     duration = models.DecimalField(decimal_places=2, max_digits=9, null=True)
@@ -86,6 +86,7 @@ class Video(models.Model):
     video_url = models.CharField(max_length=150, null=True)
     image_url = models.CharField(max_length=150, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    document = models.CharField(max_length=500, null=True)
 
     class Meta:
         verbose_name = "Video"
