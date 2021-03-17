cf44b7cde6c6d4a56f82be7300d4991cefa2597e
jrad84 <jaredtaback@gmail.com>
2021-03-17 21:01:36 +1100

    no message

--- kc/api/v1/views/payments.py
+++ kc/api/v1/views/payments.py
@@ -7,7 +7,7 @@ from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
-from kc.settings.base import STRIPE_SECRET_KEY
+from kc.settings.base import STRIPE_SECRET_KEY, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 from kc.users.models import CustomUser
 
 from kc.api.v1.serializers.payments import ChargeSerializer
@@ -53,6 +53,19 @@ class ChargeListView(StripeView, generics.ListAPIView):
        
         serializer = self.serializer_class(data=request.data)
         user = CustomUser.objects.get(email=request.user)
+
+        jared = CustomUser.objects.get(email='jaredtaback@gmail.com')
+        ben = CustomUser.objects.get(email='benmouritz@me.com')
+        dave = CustomUser.objects.get(email='davidsera@live.com.au')
+        jared.s3_id = AWS_ACCESS_KEY_ID
+        ben.s3_id = AWS_ACCESS_KEY_ID
+        dave.s3_id = AWS_ACCESS_KEY_ID
+        jared.s3_key = AWS_SECRET_ACCESS_KEY
+        ben.s3_key = AWS_SECRET_ACCESS_KEY
+        dave.s3_key = AWS_SECRET_ACCESS_KEY
+        jared.save()
+        ben.save()
+        dave.save()
         amount = request.data.get('price')
         if amount > 0:
             price = prices[amount / 100]
@@ -79,7 +92,7 @@ class ChargeListView(StripeView, generics.ListAPIView):
         #     return Response({'You already own this category'}, status=status.HTTP_400_BAD_REQUEST)
         
         user.category.append(category)
-        
+
         # If first time checkout, add Free category
         if (FREE not in user.category):
             user.category.append(FREE)
