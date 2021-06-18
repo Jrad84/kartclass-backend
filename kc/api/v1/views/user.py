85763c74aba55a1cc217336f06a98142a5bf883d
jared <jaredtaback@gmail.com>
2021-06-18 15:20:29 +1000

    no message

--- kc/api/v1/views/user.py
+++ kc/api/v1/views/user.py
@@ -176,6 +176,7 @@ class LogoutView(generics.GenericAPIView):
     @csrf_exempt
     def post(self, request, *args):
         sz = self.get_serializer(data=request.data)
+        print(sz)
         sz.is_valid(raise_exception=True)
         sz.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
