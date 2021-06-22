02abe2b624f93a5073647e5295b49cb051d82071
jared <jaredtaback@gmail.com>
2021-06-22 19:14:18 +1000

    no message

--- kc/settings/base.py
+++ kc/settings/base.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework_simplejwt.token_blacklist',
+    # 'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
     'kc.core',
     'kc.api',
