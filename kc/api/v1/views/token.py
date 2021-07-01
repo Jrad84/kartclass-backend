ab05ec0732af396f10f790ca98b3b6aa4fa05099
jared <jaredtaback@gmail.com>
2021-07-01 19:56:58 +1000

    no message

--- kc/api/v1/views/token.py
+++ kc/api/v1/views/token.py
@@ -1,7 +1,11 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+   
+    authentication_classes = []
+    permission_classes = [permissions.AllowAny,]
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
