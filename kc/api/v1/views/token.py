ded6a50b31e19f3c960aad0a512ff7a43d977312
jared <jaredtaback@gmail.com>
2021-07-03 19:46:01 +1000

    no message

--- kc/api/v1/views/token.py
+++ kc/api/v1/views/token.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework.response import Response
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -22,16 +22,28 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     
     def validate(self, attrs):
         data = super().validate(attrs)
-        print(data)
+        # print(data)
         refresh = self.get_token(self.user)
+        
         data['refresh'] = str(refresh)
         data['access'] = str(refresh.access_token)
 
         # Add extra responses here
-        data['username'] = self.user.username
-        data['groups'] = self.user.groups.values_list('name', flat=True)
+        data['email'] = self.user.email
+        
         return data
 
 class MyTokenObtainPairView(TokenObtainPairView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+    
+
+    # def post(self, request):
+        
+    #     serializer = self.serializer_class(data=request.data)
+    #     print(serializer)
+
+    #     if serializer.is_valid():
+    #         return Response({'success': True, 'message': 'ok'}, status=status.HTTP_200_OK)
+    #     return Response({'success': False, 'message': 'fuck no'}, status=status.HTTP_400_BAD_REQUEST)
+    
