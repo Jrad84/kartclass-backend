8d997ae4e052ef4e05b47388097c53333699148e
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-09-02 22:47:11 +1000

    Update payments.py

--- kc/api/v1/views/payments.py
+++ kc/api/v1/views/payments.py
@@ -19,16 +19,12 @@ class ChargeListView(generics.ListAPIView):
     def post(self, request):
        
         user = CustomUser.objects.get(email=request.user)
-      
-        # only if checkout required
-        if request.data.get('temp') is not None:
-         
-            user.temp_cat = request.data.get('temp')
-            user.checkout = request.data.get('checkout')
+        
+        user.temp_cat = request.data.get('temp')
+        user.checkout = request.data.get('checkout')
 
         mail_list = request.data.get('mail_list')
         
-       
         if mail_list == "true":
             mail_list = True
         else:
@@ -40,7 +36,6 @@ class ChargeListView(generics.ListAPIView):
         if FREE not in user.category:
             user.category.append(FREE)
 
-       
         user.save(update_fields=["checkout", "category", "temp_cat", "is_member", "mail_list"])
 
         return Response(status=status.HTTP_202_ACCEPTED)
@@ -54,14 +49,12 @@ class PaymentSuccessView(generics.ListAPIView):
     def post(self, request):
 
         user = CustomUser.objects.get(email=request.user)
-        
-        # if user.temp_cat not in user.category:
-        user.category.append(user.temp_cat)
-        # list(set(user.category))
-        user.temp_cat = None
-        user.checkout = None
-        user.save(update_fields=["category", "temp_cat", "checkout"])
 
+        if user.temp_cat not in user.category:
+            user.category.append(user.temp_cat)
+
+        user.save(update_fields=["category"])
+ 
         return Response({'temp_cat': user.temp_cat, 'category': user.category}, status=status.HTTP_200_OK)
         
         error = "Failed to update category"
