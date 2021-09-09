c36403d8f15e24dfa408a99c33a1ed0d000d0592
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-09-09 10:19:51 +1000

    Update article.py

--- kc/api/v1/serializers/article.py
+++ kc/api/v1/serializers/article.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
-from kc.core.models import Article
+from kc.core.models import Article, Category
 
 class ArticleSerializer(serializers.ModelSerializer):
+
+    category = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Category.objects.all())
 
     class Meta:
         model = Article
@@ -10,6 +13,7 @@ class ArticleSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
         instance.slug = validated_data.get('slug', instance.slug)
+        instance.category = validated_data.get('category', instance.category)
         instance.description = validated_data.get('description', instance.description)
         instance.image = validated_data.get('image', instance.image)
         instance.document = validated_data.get('document', instance.document)
